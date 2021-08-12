06bb20e43703bc486493cf8e56639d06a2ac90b3
sgnovo8 <88598099+sgnovo8@users.noreply.github.com>
2021-08-11 23:28:02 -0400

    Update novoToken.sol

--- task_7/novoToken/novoToken/contracts/novoToken.sol
+++ task_7/novoToken/novoToken/contracts/novoToken.sol
@@ -5,11 +5,11 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract DlinhNFT is ERC721URIStorage {
+contract novoToken is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("DlinhNFT", "DLINFT") {}
+    constructor() ERC721("novoToken", "SGNNFT") {}
 
     function awardItem(address owner, string memory tokenURI)
         public
